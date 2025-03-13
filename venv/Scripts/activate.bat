6145dac668e116d387d33b04ce67636bbb9c8581
Tuey <tueychirayu@gmail.com>
2025-03-13 11:15:55 +0700

    Update requirements.txt and app files

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tueyc\CMKL Year 1\URD sem 2\recommendation_api\venv
+set VIRTUAL_ENV=C:\Users\tueyc\CMKL Year 1\URD sem 2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
